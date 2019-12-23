d557277435f2ade91ef6c23d43febd66ab0f2765
AdamMusa <adammusa2222@gmail.com>
2019-12-23 11:51:47 +0100

    update form

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -1,2 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:counter/provider_resource.dart';
+import 'package:provider/provider.dart';
 
+void main() {
+  runApp(MaterialApp(
+    title: '',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: ChangeNotifierProvider<Counter>(
+      builder: (context) => Counter(),
+      child: HomePage(),
+    ),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50),
+            ),
+            Text(
+              'Vous avez appuyé :${counter.value}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: counter.Increment,
+        child: Icon(Icons.add),
+        tooltip: 'Increment',
+      ),
+    );
+  }
+}
